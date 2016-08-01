f25e7f5136d25afe88710d388db8a33f87c3badf
ganlv <331531997@qq.com>
2016-08-01 19:38:22 +0800

    New feature 'send e-mail'. API improved.

---
+++ api/config.sample.php
@@ -0,0 +1,16 @@
+<?php
+// CSV文件名设置，最好设置成别人猜不到的名字
+define('DATA_FILE', 'data.csv');
+// 防刷的记录文件
+define('IP_FILE', 'ip.php');
+// 书院名
+define('COLLEGE', array('彭康','仲英','南洋','文治','崇实','宗濂','励志','启德'));
+// 部门名
+define('GROUP', array('空','市场部','公关部','信息部门','产品部','前端美工部','后台WEB部','APP部','EUX'));
+// 邮件服务器设置，全部注释掉代表不发送邮件
+// define('MAIL_SERVER', array(
+//	 'Host'     => 'smtp.qq.com',
+//	 'Port'     => 465,
+//	 'Username' => '10000@qq.com',
+//	 'Password' => 'password',
+// ));
