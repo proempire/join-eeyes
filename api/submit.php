c26a894b9f2d04285d77a0ed51a99aac506fefc5
ganlv <331531997@qq.com>
2016-08-13 23:14:59 +0800

    API changed, new demo html, bug fixed

--- api/submit.php
+++ api/submit.php
@@ -58,7 +58,7 @@ function I($name, $type, $filter)
         exit('-3');
     }
     $data = $_POST[$name];
-    if (is_array($data)) {
+    if (!is_string($data)) {
         exit('-3');
     }
     if (is_callable($filter)) {
@@ -77,55 +77,80 @@ function I($name, $type, $filter)
 // 验证输入数据
 // 姓名：1-10个字符
 // 性别：0女、1男
+// 出生日期：YYYY-mm-dd，出生年份必须在1986年-2006年之间，且日期必须真实存在
 // 籍贯：0-40个字符
-// 出生日期：yyyy-mm-dd
-// 专业班级：1-20个字符
 // 书院；0彭康、1仲英、2南洋、3文治、4崇实、5宗濂、6励志、7启德
+// 专业班级：1-20个字符
 // 手机：号段130、131、132、133、134、135、136、137、138、139、145、147、149、150、151、152、153、155、156、157、158、159、170、175、176、177、178、180、181、182、183、184、185、186、187、188、189
 // QQ号：5-11位数字，首位不为0
-// 邮箱：username@example.com
-// 第一志愿：1市场部、2公关部、3信息部门、4产品部、5前端美工部、6后台WEB部、7APP部、8EUX(不面向新生)
-// 第二志愿：0空、1市场部、2公关部、3信息部门、4产品部、5前端美工部、6后台WEB部、7APP部、8EUX(不面向新生)
-// 个人简介：0-255个字符
-// 加入理由：0-255个字符
+// 邮箱：filter_var->checkdnsrr
+// 第一志愿：         1新闻部、2新媒体部、3影视部、4市场部、5公关部、6产品部、7app组、8web组、9前端美工组
+// 第二志愿：0(未选)、1新闻部、2新媒体部、3影视部、4市场部、5公关部、6产品部、7app组、8web组、9前端美工组，第二志愿必须与第一志愿不同
+// 个人陈述：0-255个字符
 $name = I('name', 's', '/^.{1,10}$/');
 $gender = I('gender', 'd', '/^[01]$/');
-$home = I('home', 's', '/^.{0,40}$/');
 $date = I('date', 's', function ($date) {
-    return (is_string($date)
-        && 1 === preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2})$/', $date, $matches)
-        && checkdate($matches[2], $matches[3], $matches[1])) ? $date : null;
+    if (!is_string($date)) {
+        exit('-3');
+    }
+    if (1 !== preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2})$/', $date, $matches)) {
+        exit('-3');
+    }
+    $year = (int)$matches[1];
+    if ($year < 1986 || $year > 2006) {
+        exit('-3');
+    }
+    if (!checkdate((int)$matches[2], (int)$matches[3], $year)) {
+        exit('-3');
+    }
+    return $date;
 });
-$class = I('class', 's', '/^.{1,20}$/');
+$home = I('home', 's', '/^.{0,40}$/');
 $college = I('college', 'd', '/^[0-7]$/');
+$class = I('class', 's', '/^.{1,20}$/');
 $tel = I('tel', 's', '/^(1((3\d)|(4[579])|(5[012356789])|(7[05678])|(8\d))\d{8})$/');
 $qq = I('qq', 's', '/^[1-9]\d{4,10}$/');
-$mail = I('mail', 's', '/^(|([a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+))$/');
-$first = I('first', 'd', '/^[1-8]$/');
-$second = I('second', 'd', '/^[0-8]$/');
+$mail = I('mail', 's', function ($mail) {
+    if (!is_string($mail)) {
+        exit('-3');
+    }
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        exit('-3');
+    }
+    $host = explode('@', $mail);
+    $host = $host[count($host) - 1];
+    if (!checkdnsrr($host, 'MX')) {
+        exit('-3');
+    }
+    return $mail;
+});
+$first = I('first', 'd', '/^[1-9]$/');
+$second = I('second', 'd', '/^[0-9]$/');
 if ($first === $second) {
     exit('-3');
 }
 $info = I('info', 's', '/^.{0,255}$/');
-$reason = I('reason', 's', '/^.{0,255}$/');
 // value->名称转换
 $college = COLLEGE[$college];
 $first = GROUP[$first];
 $second = GROUP[$second];
-// 时间：yyyy-mm-dd hh:mm:ss
+// 时间：YYYY-mm-dd HH:ii:ss
 $time = date('Y-m-d H:i:s');
+
 // 将csv文件写入缓存
 ob_start();
 $f = fopen('php://output', 'w');
 // 如果不存在，新建，并写入表头
-if (!file_exists(DATA_FILE) && false === fputcsv($f, array('提交时间', 'IP', '姓名', '性别', '籍贯', '出生日期', '专业班级', '书院', '手机', 'QQ号', '邮箱', '第一志愿', '第二志愿', '个人简介', '加入理由'))
+if (!file_exists(DATA_FILE) && false === fputcsv($f, TABLE_HEADER)
 ) {
     fclose($f);
+    ob_end_clean();
     exit('-4');
 }
 // 追加数据
-if (false === fputcsv($f, array($time, $ip, $name, $gender, $home, $date, $class, $college, $tel, $qq, $mail, $first, $second, $info, $reason))) {
+if (false === fputcsv($f, array($time, $client_ip, $name, $gender, $date, $home, $college, $class, $tel, $qq, $mail, $first, $second, $info))) {
     fclose($f);
+    ob_end_clean();
     exit('-4');
 }
 // Excel仅识别GBK编码的csv
@@ -133,6 +158,7 @@ file_put_contents(DATA_FILE, iconv('utf-8', 'GBK//IGNORE', ob_get_clean()), FILE
 fclose($f);
 // 未设置MAIL_SERVER即为不发送邮件
 if (defined('MAIL_SERVER')) {
+    $maskedname = mb_substr(trim($name), 0, 1) . '同学';
     // 加载PHPMailer库
     require 'PHPMailer/class.phpmailer.php';
     require 'PHPMailer/class.smtp.php';
@@ -147,12 +173,12 @@ if (defined('MAIL_SERVER')) {
     $phpmailer->Password = MAIL_SERVER['Password'];
     // 邮件设置
     $phpmailer->setFrom($phpmailer->Username, '西安交通大学e瞳网');
-    $phpmailer->addAddress($mail, $name);
+    $phpmailer->addAddress($mail, $maskedname);
     $phpmailer->isHTML(true);
     // 邮件内容
     $phpmailer->Subject = 'e瞳网招新报名反馈';
-    $phpmailer->Body = '<h1>' . htmlspecialchars($name) . ' 同学：</h1><p>你好，</p><p>小瞳已经收到您的报名申请，</p><p>经过审核后将以邮件和短信形式通知答辩地点</p>';
-    $phpmailer->AltBody = $name . ' 同学：你好，小瞳已经收到您的报名申请，经过审核后将以邮件和短信形式通知答辩地点';
+    $phpmailer->Body = '<h1>' . htmlspecialchars($maskedname) . '：</h1><p>你好，</p><p>小瞳已经收到您的报名申请，</p><p>经过审核后将以邮件和短信形式通知答辩地点</p>';
+    $phpmailer->AltBody = $maskedname . '：你好，小瞳已经收到您的报名申请，经过审核后将以邮件和短信形式通知答辩地点';
     // 发送邮件
     if (!$phpmailer->send()) {
         // 提交数据成功，发邮件失败
