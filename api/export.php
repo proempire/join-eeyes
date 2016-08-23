c8e51dd26273abdec065546a9cc60faa6f20acb5
ganlv <331531997@qq.com>
2016-08-23 11:15:19 +0800

    fix bug

--- api/export.php
+++ api/export.php
@@ -5,5 +5,5 @@ if (isset($_POST['password']) && $_POST['password'] == PASSWORD) {
 	header('Content-Disposition: attachment; filename=' . DATA_FILE);
 	readfile(DATA_FILE);
 } else {
-	echo '<form action="download.php" method="post">password:<input name="password"><input type="submit"></form>';
+	echo '<form action="' . basename(__FILE__) . '" method="post">password:<input name="password"><input type="submit"></form>';
 }
